bf3a293a02dae97956df6f12b8568e35f7f1a45a
mobrocka <mobrocka@ska.ac.za>
2016-09-09 13:40:51 +0200

    clean up

--- test_guppi.py
+++ test_guppi.py
@@ -1,26 +1,26 @@
 from guppi import *
 from pprint import pprint
 
+
 def test_guppi():
+    filename = 'blc1_guppi_57388_HIP113357_0010.0002.raw'
 
-	filename = 'blc1_guppi_57388_HIP113357_0010.0002.raw'
+    guppi = GuppiRaw(filename)
 
-	guppi = GuppiRaw(filename)
+    print guppi
 
-	print guppi
+    header = guppi.read_first_header()
 
-	header = guppi.read_first_header()
-	
-	pprint(header)
+    pprint(header)
 
-	guppi.read_header()
-	guppi.find_n_data_blocks()
-	header, idx = guppi.read_header()
-	header, data = guppi.read_next_data_block()
-	header, data = guppi.read_next_data_block()
-	header, data = guppi.read_next_data_block()
-	guppi.reset_index()
+    guppi.read_header()
+    guppi.find_n_data_blocks()
+    header, idx = guppi.read_header()
+    header, data = guppi.read_next_data_block()
+    header, data = guppi.read_next_data_block()
+    header, data = guppi.read_next_data_block()
+    guppi.reset_index()
 
 
 if __name__ == "__main__":
-	test_guppi()
+    test_guppi()
